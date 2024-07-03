dc4ed1c0d6688c3e233cb66afb86d6976a0e7e3b
Hazman <hazman0104@gmail.com>
2024-07-03 13:46:11 +0800

    about

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import { Container, Row, Col } from "react-bootstrap";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const About = () => {
+  return (
+    <section className="about" id="about">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div style={{ marginBottom: '50px' }}>
+                <h2>About Me</h2>
+                <p>Third-year Telecommunication Engineering student with a focus on Network engineering, also proficient in Graphic Design. Passionate and dedicated network engineer with a hands-on approach to problem-solving. My journey began in 2018, evolving through practical experiences and academic pursuits. also experienced as a graphic designer from various organizations.</p>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -60,10 +60,6 @@ export const Projects = () => {
     { title: "Poster",  imgUrl: designImg11},
     { title: "Manipulation",  imgUrl: designImg12},
   ];
-  
-  
-  
-  
 
   return (
     <section className="project" id="projects">
@@ -73,8 +69,6 @@ export const Projects = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div style={{ marginBottom: '50px' }}>
-                <h2>About Me</h2>
-                <p>Third-year Telecommunication Engineering student with a focus on Network engineering, also proficient in Graphic Design. Passionate and dedicated network engineer with a hands-on approach to problem-solving. My journey began in 2018, evolving through practical experiences and academic pursuits. also experienced as a graphic designer from various organizations.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
